3f697cdbae924e20d1864f18a76723a72a94ade0
Xukun <xukunxiang@uchicago.edu>
2021-01-05 14:26:04 -0600

    DEV-481 update test

--- psql-users.sh
+++ psql-users.sh
@@ -1,3 +1,2 @@
 psql -U postgres -c "create user test with superuser password 'test';"
 psql -U postgres -c "create database automated_test with owner test;"
-psql -U postgres -c "create database big_int_test with owner test;"
--- tests/test_big_int_id.py
+++ tests/test_big_int_id.py
@@ -1,71 +1,22 @@
-from copy import deepcopy
-from sqlalchemy import inspect
-from sqlalchemy.types import BIGINT
+import pytest
 
-from gdc_ng_models.snacks import database as db
-from gdc_ng_models.models import (
-    misc,
-    notifications,
-    qcreport,
-    redaction,
-    released_data,
-    reports,
-    studyrule,
-    submission,
-)
+from gdc_ng_models.models import notifications, released_data
 
 
-TARGETED_TABLE_NAMES = [
-    'filereport',
-    'notifications',
-    'qc_validation_results',
-    'qc_test_runs',
-    'redaction_log',
-    "released_data_log",
-    'gdc_reports',
-    "study_rule",
-    'transaction_logs',
-    'transaction_documents',
-]
+@pytest.mark.usefixtures("create_released_data_db")
+def test_big_int_id(db_session):
+    large_int = 1234567890123456
 
-
-def test_biginteger_id_column(db_configs):
-    """Check the 'id' column of specific tables are BIGINT
-
-    One assumption is that:
-    for each of these tables, the `id` column is the primary_key and they are monotonically increasing
-
-    some tables may have `id` in TEXT or other data_types which testing subjects here
-    """
-    new_configs = deepcopy(db_configs)
-    new_configs["database"] = "big_int_test"
-    engine = db.postgres_engine_factory(new_configs)
-
-    misc.Base.metadata.create_all(engine)
-    notifications.Base.metadata.create_all(engine)
-    qcreport.Base.metadata.create_all(engine)
-    redaction.Base.metadata.create_all(engine)
-    released_data.Base.metadata.create_all(engine)
-    reports.Base.metadata.create_all(engine)
-    studyrule.Base.metadata.create_all(engine)
-    submission.Base.metadata.create_all(engine)
-
-    insp = inspect(engine)
-    table_names = insp.get_table_names()
-    table_columns_details = {}
-    for table in table_names:
-        table_columns_details[table] = {
-            col['name']: col['type'] for col in insp.get_columns(table)
-        }
-
-    misc.Base.metadata.drop_all(engine)
-    notifications.Base.metadata.drop_all(engine)
-    qcreport.Base.metadata.drop_all(engine)
-    redaction.Base.metadata.drop_all(engine)
-    released_data.Base.metadata.drop_all(engine)
-    reports.Base.metadata.drop_all(engine)
-    studyrule.Base.metadata.drop_all(engine)
-    submission.Base.metadata.drop_all(engine)
-
-    for table in TARGETED_TABLE_NAMES:
-        assert isinstance(table_columns_details[table]['id'], BIGINT)
+    rdl_node = released_data.ReleasedDataLog(
+        id=large_int,
+        program_name="DummyProgram",
+        project_code="DummyProject",
+        release_number="dummy",
+        data_type="cnv",
+        is_open=True,
+        action="release"
+    )
+    db_session.add(rdl_node)
+    db_session.commit()
+    node = db_session.query(released_data.ReleasedDataLog).first()
+    assert node.id == large_int
