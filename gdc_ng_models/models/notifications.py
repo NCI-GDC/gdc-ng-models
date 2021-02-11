816bf78168475236cbb5be3a37d3631a75b54611
Jiakung Feng <69171880+jiakf@users.noreply.github.com>
2021-02-11 09:33:35 -0600

    Feat/dev 485 notifications datetimes (#33)
    DEV-485: add start_date and end_date to Notifications model
    
    both dates are nullable to account for existing data without those columns

--- gdc_ng_models/models/notifications.py
+++ gdc_ng_models/models/notifications.py
@@ -18,8 +18,14 @@ class Notification(Base):
     level = Column(String)
     dismissible = Column(Boolean, default=True)
     created = Column(
-        DateTime(timezone=True), nullable=False, server_default=text("now()"),
+        DateTime(timezone=True),
+        nullable=False,
+        server_default=text("now()"),
     )
+    start_date = Column(DateTime(timezone=True), nullable=True)
+    end_date = Column(DateTime(timezone=True), nullable=True)
+
+    __mapper_args__ = {"eager_defaults": True}
 
     def __repr__(self):
         return "<Notification(id='{}', level='{}', message='{}')>".format(
@@ -28,6 +34,10 @@ class Notification(Base):
 
     def to_dict(self):
         """Returns a dictionary representation of :class:`Notification`"""
+        start_date = (
+            self.start_date.isoformat() if self.start_date is not None else None
+        )
+        end_date = self.end_date.isoformat() if self.end_date is not None else None
 
         return {
             "id": self.id,
@@ -36,6 +46,8 @@ class Notification(Base):
             "dismissible": self.dismissible,
             "message": self.message,
             "level": self.level,
+            "start_date": start_date,
+            "end_date": end_date,
         }
 
     def to_json(self):
