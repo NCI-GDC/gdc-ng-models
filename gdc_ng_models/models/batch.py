a80d349760c7c01dbcc066a5bb61aa54c62ccd6c
Jiakung Feng <69171880+jiakf@users.noreply.github.com>
2021-03-17 11:20:54 -0500

    DEV-551 add Batch and BatchMembership tables (#37)
    * DEV-551 add Batch and BatchMembership tables
    
    * DEV-551 pin cryptography
    
    * DEV-551 remove description column
    
    * DEV-551 make primary/foreign keys, indexes explicit
    
    * DEV-551 remove cascading delete logic
    
    * DEV-551 add audit columns and fix repr/to_json for potential null
    datetimes
    
    * DEV-551 add updated_datetime tests
    
    * DEV-551 refactor tests
    
    * DEV-551 add repr tests
    
    * DEV-551 change String to Text columns
    
    * DEV-551 add batch status column
    
    * DEV-551 remove equality overrides
    
    * DEV-551 remove node_type and fixed some tests
    
    * DEV-551 add default lazy strategy for batch members
    
    * DEV-551 fix imports order
    
    * DEV-551 refactor tests
    
    * DEV-551 change status to Text column with validates() decorator
    
    * DEV-551 fix typo
    
    * DEV-551 add valid statuses constant

---
+++ gdc_ng_models/models/batch.py
@@ -0,0 +1,159 @@
+import json
+
+import sqlalchemy
+from sqlalchemy import orm
+from sqlalchemy.ext import declarative
+from sqlalchemy.sql import schema
+
+from gdc_ng_models.models import audit
+
+Base = declarative.declarative_base()
+
+
+class Batch(Base, audit.AuditColumnsMixin):
+    """Batch class to represent a collection of nodes.
+
+    Attributes:
+        id: unique identifier for the batch
+        name: name given to the batch
+        project_id: project the batch is a part of
+        created_datetime: the date and time when the batch is created
+        updated_datetime: the date and time when the batch was last updated
+    """
+
+    __tablename__ = "batch"
+    __mapper_args__ = {"eager_defaults": True}
+    __table_args__ = (
+        schema.PrimaryKeyConstraint("id", name="batch_pk"),
+        schema.Index("batch_name_idx", "name"),
+        schema.Index("batch_project_id_idx", "project_id"),
+        schema.Index("batch_status_idx", "status"),
+    )
+
+    VALID_STATUSES = ("OPEN", "CLOSED")
+
+    # start sequence at high number to avoid collisions with existing batch_id
+    id_seq = sqlalchemy.Sequence("batch_id_seq", metadata=Base.metadata, start=1000)
+
+    id = sqlalchemy.Column(
+        sqlalchemy.BigInteger,
+        nullable=False,
+        server_default=id_seq.next_value(),
+    )
+    name = sqlalchemy.Column(sqlalchemy.Text, nullable=False)
+    project_id = sqlalchemy.Column(sqlalchemy.Text, nullable=False)
+    status = sqlalchemy.Column(sqlalchemy.Text, default="OPEN", nullable=False)
+
+    members = orm.relationship(
+        "BatchMembership", back_populates="batch", lazy="selectin"
+    )
+
+    @orm.validates("status")
+    def validate_status(self, key, status):
+        if not status:
+            raise ValueError("status is required")
+
+        status = status.upper()
+
+        if status not in self.VALID_STATUSES:
+            raise ValueError("invalid status specified")
+
+        return status
+
+    def __repr__(self):
+        created_datetime = (
+            self.created_datetime.isoformat() if self.created_datetime else None
+        )
+        updated_datetime = (
+            self.updated_datetime.isoformat() if self.updated_datetime else None
+        )
+
+        return "<Batch(id='{}', name='{}', project_id='{}', status='{}', created_datetime='{}', updated_datetime='{}')>".format(
+            self.id,
+            self.name,
+            self.project_id,
+            self.status,
+            created_datetime,
+            updated_datetime,
+        )
+
+    def to_dict(self):
+        created_datetime = (
+            self.created_datetime.isoformat() if self.created_datetime else None
+        )
+        updated_datetime = (
+            self.updated_datetime.isoformat() if self.updated_datetime else None
+        )
+
+        return {
+            "id": self.id,
+            "name": self.name,
+            "project_id": self.project_id,
+            "status": self.status,
+            "created_datetime": created_datetime,
+            "updated_datetime": updated_datetime,
+        }
+
+    def to_json(self):
+        """Returns a JSON safe representation of a batch"""
+        return json.loads(json.dumps(self.to_dict()))
+
+
+class BatchMembership(Base, audit.AuditColumnsMixin):
+    """Membership class to represent which nodes belong in a batch.
+
+    Attributes:
+        batch_id: id of the batch
+        node_id: id of the node
+        created_datetime: the date and time when node was added to batch
+        updated_datetime: the date and time when node membership was last updated
+    """
+
+    __tablename__ = "batch_membership"
+    __table_args__ = (
+        schema.PrimaryKeyConstraint("batch_id", "node_id", name="batch_membership_pk"),
+        schema.ForeignKeyConstraint(
+            ("batch_id",),
+            ("batch.id",),
+            name="batch_membership_batch_id_fk",
+        ),
+    )
+
+    batch_id = sqlalchemy.Column(sqlalchemy.BigInteger, nullable=False)
+    node_id = sqlalchemy.Column(sqlalchemy.Text, nullable=False)
+
+    batch = orm.relationship("Batch", back_populates="members")
+
+    def __repr__(self):
+        created_datetime = (
+            self.created_datetime.isoformat() if self.created_datetime else None
+        )
+        updated_datetime = (
+            self.updated_datetime.isoformat() if self.updated_datetime else None
+        )
+
+        return "<BatchMembership(batch_id='{}', node_id='{}', created_datetime='{}', updated_datetime='{}')>".format(
+            self.batch_id,
+            self.node_id,
+            created_datetime,
+            updated_datetime,
+        )
+
+    def to_dict(self):
+        created_datetime = (
+            self.created_datetime.isoformat() if self.created_datetime else None
+        )
+        updated_datetime = (
+            self.updated_datetime.isoformat() if self.updated_datetime else None
+        )
+
+        return {
+            "batch_id": self.batch_id,
+            "node_id": self.node_id,
+            "created_datetime": created_datetime,
+            "updated_datetime": updated_datetime,
+        }
+
+    def to_json(self):
+        """Returns a JSON safe representation of a membership object"""
+        return json.loads(json.dumps(self.to_dict()))
---
+++ tests/models/test_batch_models.py
@@ -0,0 +1,498 @@
+import datetime
+import json
+
+import pytest
+import pytz
+from sqlalchemy import exc
+
+from gdc_ng_models.models import batch
+
+
+@pytest.fixture(scope="function")
+def test_batches(create_batch_db, db_session):
+    b1 = batch.Batch(name="a", project_id="GDC-MISC")
+    b2 = batch.Batch(name="b", project_id="GDC-INTERNAL")
+    db_session.add(b1)
+    db_session.add(b2)
+    db_session.commit()
+
+    yield (b1, b2)
+
+
+@pytest.fixture(scope="function")
+def test_batches_with_members(create_batch_db, db_session):
+    b1 = batch.Batch(name="a", project_id="GDC-MISC")
+    b2 = batch.Batch(name="b", project_id="GDC-INTERNAL")
+    db_session.add(b1)
+    db_session.add(b2)
+    db_session.commit()
+
+    b1.members.append(batch.BatchMembership(node_id="node_1"))
+    b1.members.append(batch.BatchMembership(node_id="node_2"))
+    b2.members.append(batch.BatchMembership(node_id="node_3"))
+    b2.members.append(batch.BatchMembership(node_id="node_4"))
+    db_session.commit()
+
+    yield (b1, b2)
+
+
+def test_batch__defaults_unique_ids(create_batch_db, db_session):
+    db_session.add(batch.Batch(name="a", project_id="GDC-MISC"))
+    db_session.add(batch.Batch(name="b", project_id="GDC-MISC"))
+    db_session.commit()
+
+    first = db_session.query(batch.Batch).filter(batch.Batch.name == "a").one()
+    second = db_session.query(batch.Batch).filter(batch.Batch.name == "b").one()
+    assert first.id is not None
+    assert second.id is not None
+    assert first.id != second.id
+
+
+@pytest.mark.parametrize(
+    "attribute",
+    ["created_datetime", "updated_datetime"],
+    ids=["default_created_datetime", "default_updated_datetime"],
+)
+def test_batch__default_datetimes(create_batch_db, db_session, attribute):
+    db_session.add(batch.Batch(name="a", project_id="GDC-MISC"))
+    db_session.commit()
+    b = db_session.query(batch.Batch).filter(batch.Batch.name == "a").one()
+    date = getattr(b, attribute)
+    assert date is not None
+
+
+def test_batch__default_status(create_batch_db, db_session):
+    db_session.add(batch.Batch(name="a", project_id="GDC-MISC"))
+    db_session.commit()
+    b = db_session.query(batch.Batch).filter(batch.Batch.name == "a").one()
+    assert b.status == "OPEN"
+
+
+@pytest.mark.parametrize(
+    "status, expected",
+    [("PENDING", "invalid status specified"), ("", "status is required")],
+    ids=["incorrect_status", "empty_status"],
+)
+def test_batch__status_values(create_batch_db, db_session, status, expected):
+    with pytest.raises(ValueError, match=r"{}".format(expected)):
+        db_session.add(batch.Batch(name="a", project_id="GDC-MISC", status=status))
+        db_session.commit()
+
+
+def test_batch__updates(create_batch_db, db_session):
+    b = batch.Batch(name="a", project_id="GDC-MISC")
+    db_session.add(b)
+    db_session.commit()
+    b.project_id = "GDC-INTERNAL"
+    db_session.commit()
+
+    updated_b = db_session.query(batch.Batch).filter(batch.Batch.name == "a").one()
+    assert updated_b.project_id == "GDC-INTERNAL"
+    assert updated_b.updated_datetime > updated_b.created_datetime
+
+
+def test_batch__repr():
+    b = batch.Batch(
+        id=1000,
+        name="a",
+        project_id="GDC-MISC",
+        status="CLOSED",
+        created_datetime=datetime.datetime(
+            year=2021,
+            month=1,
+            day=18,
+            hour=9,
+            minute=30,
+            second=10,
+            microsecond=123,
+            tzinfo=pytz.utc,
+        ),
+        updated_datetime=datetime.datetime(
+            year=2021,
+            month=1,
+            day=18,
+            hour=9,
+            minute=30,
+            second=10,
+            microsecond=123,
+            tzinfo=pytz.utc,
+        ),
+    )
+    expected = "<Batch(id='1000', name='a', project_id='GDC-MISC', status='CLOSED', created_datetime='2021-01-18T09:30:10.000123+00:00', updated_datetime='2021-01-18T09:30:10.000123+00:00')>"
+    assert repr(b) == expected
+
+
+def test_batch__primary_key_constraint(create_batch_db, db_session):
+    db_session.add(batch.Batch(id=1000, name="a", project_id="GDC-MISC"))
+    db_session.commit()
+
+    with pytest.raises(exc.IntegrityError, match=r"batch_pk"):
+        db_session.add(batch.Batch(id=1000, name="b", project_id="GDC-INTERNAL"))
+        db_session.commit()
+
+
+@pytest.mark.parametrize(
+    "contents",
+    [
+        {"project_id": "GDC-MISC"},
+        {"name": "a"},
+    ],
+    ids=["name_must_be_defined", "project_id_must_be_defined"],
+)
+def test_batch__required_fields(create_batch_db, db_session, contents):
+    with pytest.raises(exc.IntegrityError, match=r"violates not-null constraint"):
+        db_session.add(batch.Batch(**contents))
+        db_session.commit()
+
+
+@pytest.mark.parametrize(
+    "contents, expected",
+    [
+        (
+            {
+                "id": 1000,
+                "name": "a",
+                "project_id": "GDC-MISC",
+                "status": "CLOSED",
+                "created_datetime": datetime.datetime(
+                    year=2021,
+                    month=1,
+                    day=18,
+                    hour=9,
+                    minute=30,
+                    second=10,
+                    microsecond=123,
+                    tzinfo=pytz.utc,
+                ),
+                "updated_datetime": datetime.datetime(
+                    year=2021,
+                    month=1,
+                    day=18,
+                    hour=10,
+                    minute=30,
+                    second=10,
+                    microsecond=123,
+                    tzinfo=pytz.utc,
+                ),
+            },
+            json.loads(
+                json.dumps(
+                    {
+                        "id": 1000,
+                        "name": "a",
+                        "project_id": "GDC-MISC",
+                        "status": "CLOSED",
+                        "created_datetime": "2021-01-18T09:30:10.000123+00:00",
+                        "updated_datetime": "2021-01-18T10:30:10.000123+00:00",
+                    }
+                )
+            ),
+        ),
+        (
+            {
+                "id": 1000,
+                "name": "a",
+                "project_id": "GDC-MISC",
+            },
+            json.loads(
+                json.dumps(
+                    {
+                        "id": 1000,
+                        "name": "a",
+                        "project_id": "GDC-MISC",
+                        "status": None,
+                        "created_datetime": None,
+                        "updated_datetime": None,
+                    }
+                )
+            ),
+        ),
+    ],
+    ids=["with_datetimes", "no_datetimes"],
+)
+def test_batch__to_json(contents, expected):
+    b = batch.Batch(**contents)
+
+    assert b.to_json() == expected
+
+
+def test_batch_membership__direct_create(create_batch_db, db_session, test_batches):
+    b = test_batches[0]
+    db_session.add(batch.BatchMembership(batch_id=b.id, node_id="node_1"))
+    db_session.commit()
+
+    bm = (
+        db_session.query(batch.BatchMembership)
+        .filter(batch.BatchMembership.batch_id == b.id)
+        .one()
+    )
+
+    assert bm.batch_id == b.id
+    assert bm.node_id == "node_1"
+    assert bm.created_datetime is not None
+    assert bm.updated_datetime is not None
+    assert len(b.members) == 1
+
+
+def test_batch_membership__indirect_create(create_batch_db, db_session, test_batches):
+    b = test_batches[0]
+    b.members.append(batch.BatchMembership(node_id="node_1"))
+    db_session.commit()
+
+    bm = (
+        db_session.query(batch.BatchMembership)
+        .filter(batch.BatchMembership.batch_id == b.id)
+        .one()
+    )
+
+    assert bm.batch_id == b.id
+    assert bm.node_id == "node_1"
+    assert bm.created_datetime is not None
+    assert bm.updated_datetime is not None
+    assert len(b.members) == 1
+
+
+@pytest.mark.parametrize(
+    "attribute",
+    ["created_datetime", "updated_datetime"],
+    ids=["default_created_datetime", "default_updated_datetime"],
+)
+def test_batch_membership__default_datetimes(
+    create_batch_db, db_session, test_batches, attribute
+):
+    b = test_batches[0]
+    db_session.add(batch.BatchMembership(batch_id=b.id, node_id="node_1"))
+    db_session.commit()
+    b = db_session.query(batch.Batch).filter(batch.Batch.name == "a").one()
+    date = getattr(b, attribute)
+    assert date is not None
+
+
+def test_batch_membership__updated_datetime(
+    create_batch_db, db_session, test_batches_with_members
+):
+    b1 = test_batches_with_members[0]
+    b2 = test_batches_with_members[1]
+
+    db_session.query(batch.BatchMembership).filter(
+        batch.BatchMembership.node_id == "node_1"
+    ).update({"batch_id": b2.id})
+    db_session.commit()
+
+    bm = (
+        db_session.query(batch.BatchMembership)
+        .filter(batch.BatchMembership.node_id == "node_1")
+        .one()
+    )
+    assert bm.updated_datetime > bm.created_datetime
+
+
+def test_batch_membership__move_node_between_batches(
+    create_batch_db, db_session, test_batches_with_members
+):
+    b1 = test_batches_with_members[0]
+    b2 = test_batches_with_members[1]
+    db_session.query(batch.BatchMembership).filter(
+        batch.BatchMembership.node_id == "node_1"
+    ).update({"batch_id": b2.id})
+    db_session.commit()
+
+    assert len(b1.members) == 1
+    assert len(b2.members) == 3
+
+
+def test_batch_membership__repr():
+    b = batch.BatchMembership(
+        batch_id=1000,
+        node_id="node_1",
+        created_datetime=datetime.datetime(
+            year=2021,
+            month=1,
+            day=18,
+            hour=9,
+            minute=30,
+            second=10,
+            microsecond=123,
+            tzinfo=pytz.utc,
+        ),
+        updated_datetime=datetime.datetime(
+            year=2021,
+            month=1,
+            day=18,
+            hour=9,
+            minute=30,
+            second=10,
+            microsecond=123,
+            tzinfo=pytz.utc,
+        ),
+    )
+    expected = "<BatchMembership(batch_id='1000', node_id='node_1', created_datetime='2021-01-18T09:30:10.000123+00:00', updated_datetime='2021-01-18T09:30:10.000123+00:00')>"
+    assert repr(b) == expected
+
+
+def test_batch_membership__primary_key_constraint(
+    create_batch_db, db_session, test_batches
+):
+    b = test_batches[0]
+    db_session.add(batch.BatchMembership(batch_id=b.id, node_id="node_1"))
+    db_session.commit()
+    with pytest.raises(exc.IntegrityError, match=r"batch_membership_pk"):
+        db_session.add(batch.BatchMembership(batch_id=b.id, node_id="node_1"))
+        db_session.commit()
+
+
+def test_batch_membership__foreign_key_constraint(create_batch_db, db_session):
+    with pytest.raises(exc.IntegrityError, match=r"batch_membership_batch_id_fk"):
+        db_session.add(batch.BatchMembership(batch_id=1, node_id="node_1"))
+        db_session.commit()
+
+
+def test_batch_membership__node_id_required(create_batch_db, db_session, test_batches):
+    b = test_batches[0]
+    with pytest.raises(exc.IntegrityError, match="violates not-null constraint"):
+        db_session.add(batch.BatchMembership(batch_id=b.id))
+        db_session.commit()
+
+
+@pytest.mark.parametrize(
+    "contents, expected",
+    [
+        (
+            {
+                "batch_id": 1000,
+                "node_id": "node_1",
+                "created_datetime": datetime.datetime(
+                    year=2021,
+                    month=1,
+                    day=18,
+                    hour=9,
+                    minute=30,
+                    second=10,
+                    microsecond=123,
+                    tzinfo=pytz.utc,
+                ),
+                "updated_datetime": datetime.datetime(
+                    year=2021,
+                    month=1,
+                    day=18,
+                    hour=10,
+                    minute=30,
+                    second=10,
+                    microsecond=123,
+                    tzinfo=pytz.utc,
+                ),
+            },
+            json.loads(
+                json.dumps(
+                    {
+                        "batch_id": 1000,
+                        "node_id": "node_1",
+                        "created_datetime": "2021-01-18T09:30:10.000123+00:00",
+                        "updated_datetime": "2021-01-18T10:30:10.000123+00:00",
+                    }
+                )
+            ),
+        ),
+        (
+            {
+                "batch_id": 1000,
+                "node_id": "node_1",
+            },
+            json.loads(
+                json.dumps(
+                    {
+                        "batch_id": 1000,
+                        "node_id": "node_1",
+                        "created_datetime": None,
+                        "updated_datetime": None,
+                    }
+                )
+            ),
+        ),
+    ],
+    ids=["with_datetimes", "no_datetimes"],
+)
+def test_batch__membership_to_json(contents, expected):
+    b = batch.BatchMembership(**contents)
+
+    assert b.to_json() == expected
+
+
+def test_batch_membership__node_in_multiple_batches(
+    create_batch_db, db_session, test_batches
+):
+    b1 = test_batches[0]
+    b2 = test_batches[1]
+
+    b1.members.append(batch.BatchMembership(node_id="node_1"))
+    b2.members.append(batch.BatchMembership(node_id="node_1"))
+    db_session.commit()
+
+    bm1 = (
+        db_session.query(batch.BatchMembership)
+        .filter(batch.BatchMembership.batch_id == b1.id)
+        .one()
+    )
+
+    bm2 = (
+        db_session.query(batch.BatchMembership)
+        .filter(batch.BatchMembership.batch_id == b2.id)
+        .one()
+    )
+
+    assert b1.members[0].node_id == bm1.node_id
+    assert b2.members[0].node_id == bm2.node_id
+
+
+def test_batch_membership__delete_parent(
+    create_batch_db, db_session, test_batches_with_members
+):
+    with pytest.raises(
+        AssertionError,
+        match=r"Dependency rule tried to blank-out primary key column",
+    ):
+        b = test_batches_with_members[0]
+        db_session.delete(b)
+        db_session.commit()
+
+
+def test_batch_membership__delete_members(
+    create_batch_db, db_session, test_batches_with_members
+):
+    b1 = test_batches_with_members[0]
+    b2 = test_batches_with_members[1]
+
+    db_session.query(batch.BatchMembership).filter(
+        batch.BatchMembership.batch_id == b1.id
+    ).delete()
+    db_session.commit()
+
+    assert db_session.query(batch.Batch).filter(batch.Batch.id == b1.id).one() == b1
+    assert (
+        len(
+            db_session.query(batch.BatchMembership)
+            .filter(batch.BatchMembership.batch_id == b1.id)
+            .all()
+        )
+        == 0
+    )
+    assert db_session.query(batch.Batch).filter(batch.Batch.id == b2.id).one() == b2
+    assert (
+        len(
+            db_session.query(batch.BatchMembership)
+            .filter(batch.BatchMembership.batch_id == b2.id)
+            .all()
+        )
+        == 2
+    )
+
+
+def test_batch_membership__delete_orphan(
+    create_batch_db, db_session, test_batches_with_members
+):
+    with pytest.raises(
+        AssertionError, match=r"Dependency rule tried to blank-out primary key column"
+    ):
+        b = test_batches_with_members[0]
+        b.members.pop()
+        db_session.commit()
