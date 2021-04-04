48e50d7f6358cedfd8480b41712d2c35ceb2dc2c
jiakf <jkfeng@uchicago.edu>
2021-04-04 17:54:54 -0500

    DEV-569: add migration for start/end datetimes for Notifications model

---
+++ alembic/versions/e9d53a640d5d_add_start_end_datetimes_notifications.py
@@ -0,0 +1,32 @@
+"""add start end datetimes for notifications
+
+Revision ID: e9d53a640d5d
+Revises:
+Create Date: 2020-12-11 16:06:21.960486
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = "e9d53a640d5d"  # pragma: allowlist secret
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.add_column(
+        "notifications",
+        sa.Column("start_date", sa.DateTime(timezone=True), nullable=True),
+    )
+    op.add_column(
+        "notifications",
+        sa.Column("end_date", sa.DateTime(timezone=True), nullable=True),
+    )
+
+
+def downgrade():
+    op.drop_column("notifications", "start_date")
+    op.drop_column("notifications", "end_date")
