ec135c86c1611a912228307ed2d01d4566f5a3f0
Michael Solano <96085518+mpsolano@users.noreply.github.com>
2022-07-01 13:29:30 -0500

    DEV-1194: Add CohortFilter to the cohort model and add relevant tests

--- gdc_ng_models/models/cohort.py
+++ gdc_ng_models/models/cohort.py
@@ -9,9 +9,9 @@ This model defines the properties necessary to persist cohorts.
 """
 import uuid
 
-from sqlalchemy import BigInteger, Column, ForeignKey, orm, Text
+from sqlalchemy import orm, BigInteger, Boolean, Column, ForeignKey, Text
 from sqlalchemy.ext import declarative
-from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.dialects.postgresql import JSONB, UUID
 from gdc_ng_models.models import audit
 
 Base = declarative.declarative_base()
@@ -34,7 +34,7 @@ class AnonymousContext(Base, audit.AuditColumnsMixin):
     """
 
     __tablename__ = "anonymous_context"
-    id = Column(UUID(as_uuid=True), nullable=False, primary_key=True, default=uuid.uuid4)
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     name = Column(Text, nullable=False)
 
     # establishes a one-to-many relationship with Cohort
@@ -66,23 +66,21 @@ class AnonymousContext(Base, audit.AuditColumnsMixin):
 class Cohort(Base, audit.AuditColumnsMixin):
     """A base definition for a cohort entity.
 
-    Provides the basic properties to define a cohort, including a link to the
-    latest cohort filter.
-
     Attributes:
         id: A UUID identifier for the cohort.
         context_id: The UUID of the associated context.
-        current_filter_id: The ID of the current filter defined for the cohort
         name: A user defined name for the cohort.
         created_datetime: The date and time when the record is created.
         updated_datetime: The date and time when the record is updated.
     """
 
     __tablename__ = "cohort"
-    id = Column(UUID(as_uuid=True), nullable=False, primary_key=True, default=uuid.uuid4)
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     name = Column(Text, nullable=False)
-    context_id = Column(UUID(as_uuid=True), ForeignKey("anonymous_context.id"), nullable=False, default=uuid.uuid4())
-    current_filter_id = Column(BigInteger, nullable=False)
+    context_id = Column(UUID(as_uuid=True), ForeignKey("anonymous_context.id"), nullable=False)
+
+    # establishes a one-to-many relationship with CohortFilter
+    filters = orm.relationship("CohortFilter")
 
     def __repr__(self):
         return (
@@ -90,13 +88,12 @@ class Cohort(Base, audit.AuditColumnsMixin):
             "id={id}, "
             "name='{name}', "
             "context_id={context_id}, "
-            "current_filter_id={current_filter_id}, "
+            # "current_filter_id={current_filter_id}, "
             "created_datetime={created_datetime}, "
             "updated_datetime={updated_datetime})>".format(
                 id=self.id,
                 name=self.name,
                 context_id=self.context_id,
-                current_filter_id=self.current_filter_id,
                 created_datetime=self.created_datetime.isoformat() if self.created_datetime else None,
                 updated_datetime=self.updated_datetime.isoformat() if self.updated_datetime else None,
             )
@@ -107,15 +104,70 @@ class Cohort(Base, audit.AuditColumnsMixin):
             "id": str(self.id),
             "name": self.name,
             "context_id": str(self.context_id),
-            "current_filter_id": self.current_filter_id,
             "created_datetime": self.created_datetime.isoformat() if self.created_datetime else None,
             "updated_datetime": self.updated_datetime.isoformat() if self.updated_datetime else None,
         }
 
 
-# class CohortFilter(Base, audit.AuditColumnsMixin):
-#     pass
-#
-#
+class CohortFilter(Base, audit.AuditColumnsMixin):
+    """A filter defining the set of cases in a cohort.
+
+    Provides the properties for defining a cohort filter, including the filter
+    itself, a static indicator, and a self-referential foreign key to maintain
+    a history of changes.
+
+    Attributes:
+        id: A bigint identifier for the cohort.
+        parent_id: A self-referential key to maintain parent-child history.
+        cohort_id: The ID of the cohort associated with the filter.
+        filters: A JSONB representation of the filter defining the case set
+        static: A boolean indicator to designate a filter as static.
+        created_datetime: The date and time when the record is created.
+        updated_datetime: The date and time when the record is updated.
+    """
+
+    __tablename__ = "cohort_filter"
+    id = Column(BigInteger, primary_key=True)
+    parent_id = Column(BigInteger, ForeignKey("cohort_filter.id"))
+    cohort_id = Column(UUID(as_uuid=True), ForeignKey("cohort.id"), nullable=False)
+    filters = Column(JSONB, nullable=False)
+    static = Column(Boolean, nullable=False, default=False)
+
+    # establishes an adjacency relationship (i.e. self-referential key)
+    # TODO: do we actually need make this to be one-to-one as per the design?
+    parent = orm.relationship("CohortFilter")
+
+    def __repr__(self):
+        return (
+            "<CohortFilter("
+            "id={id}, "
+            "parent_id={parent_id}, "
+            "cohort_id={cohort_id}, "
+            "filters={filters}, "
+            "static={static}, "
+            "created_datetime={created_datetime}, "
+            "updated_datetime={updated_datetime})>".format(
+                id=self.id,
+                parent_id=self.parent_id,
+                cohort_id=self.cohort_id,
+                filters=self.filters,
+                static=self.static,
+                created_datetime=self.created_datetime.isoformat() if self.created_datetime else None,
+                updated_datetime=self.updated_datetime.isoformat() if self.updated_datetime else None,
+            )
+        )
+
+    def to_json(self):
+        return {
+            "id": self.id,
+            "parent_id": self.parent_id,
+            "cohort_id": str(self.cohort_id),
+            "filters": self.filters,
+            "static": self.static,
+            "created_datetime": self.created_datetime.isoformat() if self.created_datetime else None,
+            "updated_datetime": self.updated_datetime.isoformat() if self.updated_datetime else None,
+        }
+
+
 # class CohortSnapshot(Base, audit.AuditColumnsMixin):
 #     pass
--- tests/models/test_cohort.py
+++ tests/models/test_cohort.py
@@ -7,12 +7,25 @@ from sqlalchemy import exc
 
 @pytest.fixture(scope="function")
 def fixture_context(create_cohort_db, db_session):
-    """Create an anonymous context for use with other tests."""
+    """Create an anonymous context for use with test cases."""
 
-    db_session.add(cohort.AnonymousContext(name="test_context"))
+    db_session.add(cohort.AnonymousContext(name="fixture_context"))
     db_session.commit()
     return db_session.query(cohort.AnonymousContext).filter(
-        cohort.AnonymousContext.name == "test_context").one()
+        cohort.AnonymousContext.name == "fixture_context").one()
+
+
+@pytest.fixture(scope="function")
+def fixture_cohort(create_cohort_db, db_session, fixture_context):
+    """Create a cohort for use with test cases."""
+
+    db_session.add(cohort.Cohort(
+        name="fixture_cohort",
+        context_id=fixture_context.id,
+    ))
+    db_session.commit()
+    return db_session.query(cohort.Cohort).filter(
+        cohort.Cohort.name == "fixture_cohort").one()
 
 
 def test_anonymous_context__valid_create(create_cohort_db, db_session):
@@ -103,14 +116,12 @@ def test_cohort__valid_create(create_cohort_db, db_session, fixture_context):
     expected_id = uuid.uuid4()
     expected_name = "test_cohort"
     expected_context_id = fixture_context.id
-    expected_current_filter_id = 1
 
     # create and retrieve cohort
     db_session.add(cohort.Cohort(
         id=expected_id,
         name=expected_name,
         context_id=expected_context_id,
-        current_filter_id=expected_current_filter_id,
     ))
     db_session.commit()
     test_cohort = db_session.query(cohort.Cohort).filter(
@@ -120,7 +131,6 @@ def test_cohort__valid_create(create_cohort_db, db_session, fixture_context):
     assert test_cohort.id == expected_id
     assert test_cohort.name == expected_name
     assert test_cohort.context_id == expected_context_id
-    assert test_cohort.current_filter_id == expected_current_filter_id
 
 
 def test_cohort__context_fkey_constraint(create_cohort_db, db_session):
@@ -131,7 +141,6 @@ def test_cohort__context_fkey_constraint(create_cohort_db, db_session):
         db_session.add(cohort.Cohort(
             name="test_cohort_1",
             context_id=non_existent_id,
-            current_filter_id=1,
         ))
         db_session.commit()
 
@@ -144,7 +153,6 @@ def test_cohort__unique_ids(create_cohort_db, db_session, fixture_context):
         id=target_id,
         name="test_cohort_1",
         context_id=fixture_context.id,
-        current_filter_id=1,
     ))
     db_session.commit()
 
@@ -153,7 +161,6 @@ def test_cohort__unique_ids(create_cohort_db, db_session, fixture_context):
             id=target_id,
             name="test_cohort_2",
             context_id=fixture_context.id,
-            current_filter_id=1,
         ))
         db_session.commit()
 
@@ -164,12 +171,10 @@ def test_cohort__defaults_unique_ids(create_cohort_db, db_session, fixture_conte
     db_session.add(cohort.Cohort(
         name="cohort_1",
         context_id=fixture_context.id,
-        current_filter_id=1,
     ))
     db_session.add(cohort.Cohort(
         name="cohort_2",
         context_id=fixture_context.id,
-        current_filter_id=2,
     ))
     db_session.commit()
 
@@ -190,7 +195,6 @@ def test_cohort__name_not_nullable(create_cohort_db, db_session, fixture_context
         db_session.add(cohort.Cohort(
             id=uuid.uuid4(),
             context_id=fixture_context.id,
-            current_filter_id=1,
         ))
         db_session.commit()
 
@@ -202,7 +206,6 @@ def test_cohort__to_json(create_cohort_db, db_session, fixture_context):
         name="test_cohort",
         id=uuid.uuid4(),
         context_id=fixture_context.id,
-        current_filter_id=1,
     ))
     db_session.commit()
 
@@ -213,9 +216,211 @@ def test_cohort__to_json(create_cohort_db, db_session, fixture_context):
         "id": str(test_cohort.id),
         "name": test_cohort.name,
         "context_id": str(test_cohort.context_id),
-        "current_filter_id": test_cohort.current_filter_id,
         "created_datetime": test_cohort.created_datetime.isoformat(),
         "updated_datetime": test_cohort.updated_datetime.isoformat(),
     }))
 
     assert test_cohort.to_json() == expected_json
+
+
+def test_cohort_filter__valid_create(create_cohort_db, db_session, fixture_cohort):
+    """Tests creation of a valid cohort filter entity."""
+
+    # define expected values
+    expected_id = 1
+    expected_cohort_id = fixture_cohort.id
+    expected_filters = [
+        {
+          "field": "cases.primary_site",
+          "value": ["breast", "bronchus and lung"]
+        }
+    ]
+    expected_static = False
+
+    # create and retrieve cohort
+    db_session.add(cohort.CohortFilter(
+        id=expected_id,
+        parent_id=None,
+        cohort_id=expected_cohort_id,
+        filters=expected_filters,
+        static=expected_static,
+    ))
+    db_session.commit()
+    test_filter = db_session.query(cohort.CohortFilter).filter(
+        cohort.CohortFilter.id == expected_id).one()
+
+    assert test_filter.id == expected_id
+    assert test_filter.parent_id is None
+    assert test_filter.cohort_id == expected_cohort_id
+    assert test_filter.filters == expected_filters
+    assert test_filter.static == expected_static
+
+
+def test_cohort_filter__cohort_fkey_constraint(create_cohort_db, db_session):
+    """Tests cohort filter foreign key constraint on cohort_id."""
+
+    non_existent_id = uuid.uuid4()
+    with pytest.raises(exc.IntegrityError, match=r"cohort_filter_cohort_id_fkey"):
+        db_session.add(cohort.CohortFilter(
+            id=1,
+            cohort_id=non_existent_id,
+            filters=[],
+        ))
+        db_session.commit()
+
+
+def test_cohort_filter__unique_ids(create_cohort_db, db_session, fixture_cohort):
+    """Tests unique constraint on cohort filter ID."""
+
+    target_id = 1
+    db_session.add(cohort.CohortFilter(
+        id=target_id,
+        cohort_id=fixture_cohort.id,
+        filters=[],
+    ))
+    db_session.commit()
+
+    with pytest.raises(exc.IntegrityError, match=r"cohort_filter_pkey"):
+        db_session.add(cohort.CohortFilter(
+            id=target_id,
+            cohort_id=fixture_cohort.id,
+            filters=[],
+        ))
+        db_session.commit()
+
+
+def test_cohort_filter__defaults_unique_ids(create_cohort_db, db_session, fixture_cohort):
+    """Tests default generated IDs on cohort filter are unique."""
+
+    db_session.add(cohort.CohortFilter(
+        cohort_id=fixture_cohort.id,
+        filters=[],
+    ))
+    db_session.add(cohort.CohortFilter(
+        cohort_id=fixture_cohort.id,
+        filters=[],
+    ))
+    db_session.commit()
+
+    cohorts = db_session.query(cohort.CohortFilter).filter().all()
+    assert len(cohorts) == 2
+    assert cohorts[0].id is not None
+    assert cohorts[1].id is not None
+    assert cohorts[0].id != cohorts[1].id
+
+
+def test_cohort_filter__filters_not_nullable(create_cohort_db, db_session, fixture_cohort):
+    """Tests filters must be defined for cohort filter."""
+
+    with pytest.raises(exc.IntegrityError, match=r"violates not-null constraint"):
+        db_session.add(cohort.CohortFilter(
+            cohort_id=fixture_cohort.id,
+        ))
+        db_session.commit()
+
+
+def test_cohort_filter__static_default(create_cohort_db, db_session, fixture_cohort):
+    """Tests static indicator on cohort filter defaults to false."""
+
+    db_session.add(cohort.CohortFilter(
+        id=1,
+        cohort_id=fixture_cohort.id,
+        filters=[],
+    ))
+    db_session.commit()
+
+    test_filter = db_session.query(cohort.CohortFilter).filter(
+        cohort.CohortFilter.id == 1).one()
+
+    assert test_filter.static is False
+
+
+def test_cohort_filter__parent_id_history(create_cohort_db, db_session, fixture_cohort):
+    """Tests history is maintained via parent_id on cohort filter."""
+
+    parent_id = 1
+    child_1_id = 2
+    child_2_id = 3
+
+    # create parent cohort
+    db_session.add(cohort.CohortFilter(
+        id=parent_id,
+        cohort_id=fixture_cohort.id,
+        filters=[
+            {
+              "field": "cases.primary_site",
+              "value": ["breast", "bronchus and lung"]
+            }
+        ],
+    ))
+    db_session.commit()
+
+    parent = db_session.query(cohort.CohortFilter).filter(
+        cohort.CohortFilter.id == parent_id).one()
+
+    # create first child
+    db_session.add(cohort.CohortFilter(
+        id=child_1_id,
+        parent_id=parent.id,
+        cohort_id=fixture_cohort.id,
+        filters=[
+            {
+              "field": "cases.primary_site",
+              "value": ["bronchus and lung"]
+            }
+        ],
+    ))
+    child_1 = db_session.query(cohort.CohortFilter).filter(
+        cohort.CohortFilter.id == child_1_id).one()
+
+    # create second child
+    db_session.add(cohort.CohortFilter(
+        id=child_2_id,
+        parent_id=child_1.id,
+        cohort_id=fixture_cohort.id,
+        filters=[
+            {
+              "field": "cases.primary_site",
+              "value": ["breast"]
+            }
+        ],
+    ))
+    db_session.commit()
+
+    child_2 = db_session.query(cohort.CohortFilter).filter(
+        cohort.CohortFilter.id == child_2_id).one()
+
+    assert parent.parent_id is None
+    assert child_1.parent_id == parent.id
+    assert child_2.parent_id == child_1.id
+
+
+def test_cohort_filter__to_json(create_cohort_db, db_session, fixture_cohort):
+    """Tests json output for cohort filter is valid."""
+
+    db_session.add(cohort.CohortFilter(
+        id=1,
+        cohort_id=fixture_cohort.id,
+        filters=[
+            {
+              "field": "cases.primary_site",
+              "value": ["breast", "bronchus and lung"]
+            }
+        ],
+    ))
+    db_session.commit()
+
+    test_filter = db_session.query(cohort.CohortFilter).filter(
+        cohort.CohortFilter.id == 1).one()
+
+    expected_json = json.loads(json.dumps({
+        "id": test_filter.id,
+        "parent_id": None,
+        "cohort_id": str(test_filter.cohort_id),
+        "filters": test_filter.filters,
+        "static": test_filter.static,
+        "created_datetime": test_filter.created_datetime.isoformat(),
+        "updated_datetime": test_filter.updated_datetime.isoformat(),
+    }))
+
+    assert test_filter.to_json() == expected_json
