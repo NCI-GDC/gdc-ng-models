303b17a63770a21bb98bc2f0828513fc81fb8edb
Jason Stiles <99777116+stilesj-uchicago@users.noreply.github.com>
2023-05-22 17:12:07 -0400

    DEV-1836 relax dependency versions in libraries to allow applications… (#55)
    * DEV-1836 relax dependency versions in libraries to allow applications to specify the exact version.
    
    * Remove Jenkinsfile

--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ setup(
     use_scm_version={"local_scheme": "dirty-tag", "fallback_version": "local"},
     description="Non-graph GDC models",
     install_requires=[
-        "cryptography~=3.2.1",
-        "psycopg2-binary~=2.8.2",
+        "cryptography~=3.2",
+        "psycopg2-binary~=2.8",
         "pytz~=2020.5",
-        "sqlalchemy~=1.3.3",
+        "sqlalchemy~=1.3",
     ],
     license="Apache",
     packages=find_packages(),
