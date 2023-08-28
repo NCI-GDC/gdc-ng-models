33469f6aca63474f35c701d4d21da8834eb09b21
Qiao Qiao <qiaoq@uchicago.edu>
2023-08-28 09:55:03 -0500

    update setup.py, add MANIFEST.in, add pyproject.toml

--- setup.py
+++ setup.py
@@ -1,12 +1,28 @@
-from setuptools import setup, find_packages
+# read the contents of your README file
+from pathlib import Path
+
+from setuptools import find_packages, setup
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="gdc_ng_models",
-    setup_requires=["setuptools_scm<6"],
-    use_scm_version={"local_scheme": "dirty-tag", "fallback_version": "local"},
     description="Non-graph GDC models",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="NCI GDC",
+    author_email="gdc_dev_questions-aaaaae2lhsbell56tlvh3upgoq@cdis.slack.com",
+    url="https://github.com/NCI-GDC/gdc-ng-models",
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+    license="Apache",
     install_requires=[
-        "cryptography~=3.2",
         "psycopg2~=2.9",
         "pytz~=2020.5",
         "sqlalchemy~=1.3.14",
@@ -20,7 +36,6 @@ setup(
         ],
         "alembic": ["alembic~=1.4"],
     },
-    license="Apache",
     packages=find_packages(),
     package_data={"gdc_ng_models": ["alembic/*"]},
     include_package_data=True,
