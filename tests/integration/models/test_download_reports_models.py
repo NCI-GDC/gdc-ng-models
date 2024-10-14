680f1df3ac3bcbb1933d0f28f5407dae73e55162
Henddher Pedroza <henddher@uchicago.edu>
2024-10-14 10:47:11 -0500

    Add test to verify each add_count/size_xxx method works correctly.

--- tests/integration/models/test_download_reports_models.py
+++ tests/integration/models/test_download_reports_models.py
@@ -1,6 +1,7 @@
 import json
 from datetime import date
 
+import pytest
 from cdisutils.dictionary import sort_dict
 
 from gdc_ng_models.models.download_reports import DataDownloadReport, DataUsageReport
@@ -121,4 +122,35 @@ def test_download_report_to_json_contains_size_and_count():
             "last_updated": "None"
         }
         """
+    )
+
+
+_SIZE_COUNT_PROP_MAP = {
+    "size": "downloaded_size_gb",
+    "count": "user_interest_files_count",
+}
+
+
+@pytest.mark.parametrize("size_or_count", (("size", "count")))
+@pytest.mark.parametrize(
+    "field_name",
+    ("access_type", "project_id", "access_location", "experimental_strategy"),
+)
+def test_download_report_to_json_contains_field_with_value(field_name, size_or_count):
+    report = DataDownloadReport()
+    adder_method = getattr(report, f"add_{size_or_count}_{field_name}")
+
+    adder_method(field_name, 123)
+    report.to_json() == json.loads(
+        """
+        {
+            "%(field_name)s_report": {
+                "%(size_or_count_prop)s": 123
+            }
+        }
+        """
+        % {
+            "field_name": field_name,
+            "size_or_count_prop": _SIZE_COUNT_PROP_MAP[size_or_count],
+        }
     )
