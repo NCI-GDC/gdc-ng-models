16b4fdf6e0c8a42a1910a29934116d89a59a7654
Micky Byrnes <68259544+BenX00@users.noreply.github.com>
2026-06-24 14:46:40 -0500

    Feat/dev 3704 use testcontainers (#76)

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -1,17 +1,18 @@
-"""
-gdcdatamodel.test.conftest
+"""gdcdatamodel.test.conftest
 ----------------------------------
 
 pytest setup for gdcdatamodel tests
 """
 
+import os
 import shlex
-from typing import Callable, Dict
+from collections.abc import Callable, Iterator
+from importlib import resources
 
 import pytest
-import sqlalchemy
 from sqlalchemy.engine import Engine
 from sqlalchemy.orm import sessionmaker
+from testcontainers import compose
 
 from gdc_ng_models import cli
 from gdc_ng_models.models import (
@@ -30,12 +31,25 @@ Session = sessionmaker()
 
 
 @pytest.fixture(scope="session")
-def db_configs():
-    return db.get_configs()
+def db_configs() -> Iterator[dict]:
+    if os.getenv("CI"):
+        yield db.get_configs()
+        return
+
+    docker_resource = resources.files("tests.integration") / "docker"
+
+    with (
+        resources.as_file(docker_resource) as docker_dir,
+        compose.DockerCompose(docker_dir, pull=True) as services,
+    ):
+        pg_port = services.get_service_port("postgres")
+        os.environ["PG_HOST"] = f"localhost:{pg_port}"
+
+        yield db.get_configs()
 
 
 @pytest.fixture(scope="session")
-def db_args(db_configs: Dict[str, str]) -> str:
+def db_args(db_configs: dict[str, str]) -> str:
     return (
         f"-H {db_configs['host']} -d {db_configs['database']} -u {db_configs['admin_user']} "
         f"-p {db_configs['admin_password']}"
@@ -43,7 +57,7 @@ def db_args(db_configs: Dict[str, str]) -> str:
 
 
 @pytest.fixture(scope="session")
-def db_engine(db_configs: Dict[str, str]) -> Engine:
+def db_engine(db_configs: dict[str, str]) -> Engine:
     return db.postgres_engine_factory(db_configs)
 
 
@@ -57,7 +71,7 @@ def create_reports_db(db_engine, ng_models_cli, db_args: str) -> None:
 @pytest.fixture(scope="session")
 def create_entity_set_db(
     db_engine: Engine, db_args: str, ng_models_cli: Callable[[str], None]
-) -> None:
+) -> Iterator[None]:
     """Provides capabilities for setup and teardown of a test entity_sets tables.
 
     Creates tables in a database using the declarations in the entity_set module in the
@@ -116,7 +130,6 @@ def create_batch_db(db_engine):
 
 @pytest.fixture(scope="session")
 def create_cohort_db(db_engine):
-    # type: (sqlalchemy.engine.base.Engine) -> None
     """Provides capabilities for setup and teardown of a test cohort database.
 
     Creates a database using the declarations in the cohort module in the
